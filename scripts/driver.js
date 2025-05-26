0e431f19b36478b6c1630efd8df917f1fb60f5f0
Abraham Done <andone@mac.com>
2025-05-26 12:38:26 -0600

    clean up

--- scripts/driver.js
+++ scripts/driver.js
@@ -171,13 +171,13 @@ MySample.main = (function(graphics) {
         }
         switch (speed) {
             case -1:
-                tireRotation -= 0.04;
+                tireRotation -= 0.03;
                 break;
             case 0:
                 tireRotation -= 0.05;
                 break;
             case 1:
-                tireRotation -= 0.06;
+                tireRotation -= 0.07;
                 break;
         }
 
@@ -223,35 +223,28 @@ MySample.main = (function(graphics) {
         graphics.clear();
 
         let scale = {x: scaleFactor, y: scaleFactor};
-        let transformedCar = car;
-        transformedCar = graphics.translateComplexLine(transformedCar, speedTranslate);
+
+        let transformedCar = graphics.translateComplexLine(car, speedTranslate);
         transformedCar = graphics.scaleComplexLine(transformedCar, scale);
-        let transformedTire1 = tire1;
-        transformedTire1 = graphics.translateComplexLine(transformedTire1, speedTranslate);
+        let transformedTire1 = graphics.translateComplexLine(tire1, speedTranslate);
         transformedTire1 = graphics.scaleComplexLine(transformedTire1, scale, transformedCar.center);
-        let transformedTire2 = tire2;
-        transformedTire2 = graphics.translateComplexLine(transformedTire2, speedTranslate);
+        let transformedTire2 = graphics.translateComplexLine(tire2, speedTranslate);
         transformedTire2 = graphics.scaleComplexLine(transformedTire2, scale, transformedCar.center);
-        let transformedSpoke1 = spokes1;
-        transformedSpoke1 = graphics.translateComplexLine(transformedSpoke1, speedTranslate);
+        let transformedSpoke1 = graphics.translateComplexLine(spokes1, speedTranslate);
         transformedSpoke1 = graphics.rotateComplexLine(transformedSpoke1, tireRotation);
         transformedSpoke1 = graphics.scaleComplexLine(transformedSpoke1, scale, transformedCar.center);
-        let transformedSpoke2 = spokes2;
-        transformedSpoke2 = graphics.translateComplexLine(transformedSpoke2, speedTranslate);
+        let transformedSpoke2 = graphics.translateComplexLine(spokes2, speedTranslate);
         transformedSpoke2 = graphics.rotateComplexLine(transformedSpoke2, tireRotation);
         transformedSpoke2 = graphics.scaleComplexLine(transformedSpoke2, scale, transformedCar.center);
-        let transformedWindow1 = window1;
-        transformedWindow1 = graphics.translatePrimitive(transformedWindow1, speedTranslate);
+        let transformedWindow1 = graphics.translatePrimitive(window1, speedTranslate);
         transformedWindow1 = graphics.scalePrimitive(transformedWindow1, scale, transformedCar.center);
-        let transformedWindow2 = window2;
-        transformedWindow2 = graphics.translatePrimitive(transformedWindow2, speedTranslate);
+        let transformedWindow2 = graphics.translatePrimitive(window2, speedTranslate);
         transformedWindow2 = graphics.scalePrimitive(transformedWindow2, scale, transformedCar.center);
-        let transformedBumper1 = bumper1;
-        transformedBumper1 = graphics.translatePrimitive(transformedBumper1, speedTranslate);
+        let transformedBumper1 = graphics.translatePrimitive(bumper1, speedTranslate);
         transformedBumper1 = graphics.scalePrimitive(transformedBumper1, scale, transformedCar.center);
-        let transformedBumper2 = bumper2;
-        transformedBumper2 = graphics.translatePrimitive(transformedBumper2, speedTranslate);
+        let transformedBumper2 = graphics.translatePrimitive(bumper2, speedTranslate);
         transformedBumper2 = graphics.scalePrimitive(transformedBumper2, scale, transformedCar.center);
+
         graphics.drawPrimitive(transformedBumper2, bumperColor);
         graphics.drawPrimitive(transformedBumper1, bumperColor);
         graphics.drawComplexLine(transformedCar, carColor);
